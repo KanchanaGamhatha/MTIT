b9a86fb015331986a1295d702c8187712f159f21
Kanchana Gamhatha <it12088942@my.sliit.lk>
2015-05-04 23:06:52 +0530

    Revert "final"
    This reverts commit fafa866e87100a62b8124237e5b5d04ce78af37e.

--- MTIT lab8/src/com/mtit/lab/Book.java
+++
@@ -1,9 +0,0 @@
-package com.mtit.lab;
-
-public class Book 
-{
-	private String bookName = "Head First design patterns";
-	public int bookCount = 20;
-	public String [] authors = {"Eric Freeman", "Elisabeth Freeman"};
-	public double price;
-}
